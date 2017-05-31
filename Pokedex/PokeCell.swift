aecfc31bb4058777c1eaa3acddc1072b67139d05
Zack Falgout <zfalgout@gmail.com>
2017-05-31 12:51:57 -0500

    Added a custom font, a music button, and the ability to parse a csv file of pokemon.  The pokemon are then rendered to the screen with an image and a name.

---
+++ Pokedex/PokeCell.swift
@@ -0,0 +1,32 @@
+//
+//  PokeCellCollectionViewCell.swift
+//  Pokedex
+//
+//  Created by Zack Falgout on 5/31/17.
+//  Copyright © 2017 Zack Falgout. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(_ pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLabel.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+    
+}
