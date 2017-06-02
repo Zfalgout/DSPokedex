583b115fdf7ba92dc66e94c2c53e90750aa08270
Zack Falgout <zfalgout@gmail.com>
2017-06-01 22:31:29 -0500

    Finished lecture 98.  Installed the Alamofire cocoapod and can make calls to outside APIs.  Updated the model to contain new fields.  Updated the detail veiw controller to handle the update of the UI for certain fields.  Will update the rest in future lectures.

--- Pokedex/PokemonDetailVC.swift
+++ Pokedex/PokemonDetailVC.swift
@@ -13,11 +13,45 @@ class PokemonDetailVC: UIViewController {
     var pokemon: Pokemon!
     
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var mainImage: UIImageView!
+    @IBOutlet weak var descLabel: UILabel!
+    @IBOutlet weak var typeLabel: UIStackView!
+    @IBOutlet weak var defLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var attackLabel: UILabel!
+    @IBOutlet weak var currentEvoImage: UIImageView!
+    @IBOutlet weak var nextEvoImage: UIImageView!
+    @IBOutlet weak var evoLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         nameLabel.text = pokemon.name
+        
+        pokemon.downloadPokemonDetails {
+            
+            print("Did arrive here")
+            //Whatever we write here will only be called after the network call is complete.
+            
+            self.updateUI()
+        }
     }
+    
+    func updateUI() {
+        nameLabel.text = pokemon.name
+        attackLabel.text = pokemon.attack
+        defLabel.text = pokemon.defense
+        heightLabel.text = pokemon.height
+        weightLabel.text = pokemon.weight
+        
+    }
+    
+    @IBAction func backButtonPressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
 
 }
